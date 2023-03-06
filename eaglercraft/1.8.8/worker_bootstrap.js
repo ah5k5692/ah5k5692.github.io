861b9d7725b1b5272b5e158f8c2e8056ce6d3035
Abdulhadi5692HDI2 <110974220+Abdulhadi5692HDI2@users.noreply.github.com>
2023-03-06 19:18:04 +0000

    Add files via upload

---
+++ eaglercraft/1.8.8/worker_bootstrap.js
@@ -0,0 +1,8 @@
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>nginx/1.18.0 (Ubuntu)</center>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon='{"rayId":"7a3ce1db6a07dc57","version":"2023.2.0","r":1,"token":"ef17691dd2754ad48537cd5b4ca6b69f","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
