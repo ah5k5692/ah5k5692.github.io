e893b68e8be3d9803d849920416cb55308487b33
Abdulhadi5692HDI2-3 <abdulhadi5692hdi2@gmail.com>
2023-11-19 14:06:46 +0000

    get all the secrets mate x3

--- functions/api/v1/secrets/getAllSecrets/index.js
+++ functions/api/v1/secrets/getAllSecrets/index.js
@@ -1,7 +1,10 @@
 import {getSecretId, countSecrets} from "../getSecretId.internal";
 
 export function onRequest(ctx) {
-    for (let i = 0; i < countSecrets(), i++;) {
-        return new Response(JSON.stringify({id: i, data: getSecretId(i)}));
+    let secrets = new Array(countSecrets());
+    secrets[0] = null;
+    for (let i = 1;i < countSecrets(); i++) {
+        secrets[i] = {id: i, data: getSecretId(i)};
     }
+    return JSON.stringify(secrets);
 }
