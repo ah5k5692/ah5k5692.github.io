5b395bf3d7a9b37c526f6a2718f81937c3516ac1
Abdulhadi5692HDI2-3 <abdulhadi5692hdi2@gmail.com>
2023-11-19 12:43:14 +0000

    make this thing work x2

--- functions/api/v1/getsecretid.js
+++ functions/api/v1/getsecretid.js
@@ -1,18 +1,13 @@
-class SecretException {
-    constructor(str) {
-        return new Response("Exception while trying to get Secret.\nError: " + str);
-    }
-}
 export function getSecretId(id) {
     switch (id) {
         case '1': {
-            return new Response("Hello this is a test note. Thanks for read bye bye!");
+            return ("Hello this is a test note. Thanks for read bye bye!");
         }
         case '2': {
-            return new Response("saturday or sunday?");
+            return ("saturday or sunday?");
         }
         default: {
-            return new SecretException("Secret with ID:" + id + " doesn't exist!");
+            return ("Secret with ID:" + id + " doesn't exist!");
         }
     }
 }
