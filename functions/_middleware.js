5897c25e5a69368710450ce8b248dbdcd118fe2a
Abdulhadi5692HDI2-3 <abdulhadi5692hdi2@gmail.com>
2023-11-19 16:55:42 +0000

    fix a crappy error i made

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,7 +2,7 @@ import pagesPluginStaticForms from "@cloudflare/pages-plugin-static-forms";
 
 export const onRequest = pagesPluginStaticForms({
     respondWith: ({formData, name}) => {
-        const name = formData.get('name');
+        name = formData.get('name');
         return new Response("Hello " + name + "! I hope you are having a great day!");
     }
 });
