f963a4b325914529a69d1fa84042b718f9a3db71
Abdulhadi5692HDI2-3 <abdulhadi5692hdi2@gmail.com>
2023-11-19 12:58:05 +0000

    make this thing work x5

--- functions/secrets/[secret].js
+++ functions/secrets/[secret].js
@@ -1,6 +1,6 @@
 import {getSecretId} from "../api/v1/[getSecretId]";
 export function onRequest(ctx) {
-    res = 'Secret #' + ctx.params.secret + '\nData:\n' + getSecretId(null, ctx.params.secret);
+    let res = 'Secret #' + ctx.params.secret + '\nData:\n' + getSecretId(null, ctx.params.secret);
 
     return new Response(res);
 }
